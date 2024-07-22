c060ac536ef4e33732cf8341dfe0e0bad1db5cfa
Parnab03 <pb7439578071@gmail.com>
2024-07-22 21:12:00 +0530

    code updated

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -1,16 +1,18 @@
-import { Fragment } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    let foodItems = ['Dal','Vegiii','Roti','Salad','Milk']
+    let foodItems = ["Dal", "Vegiii", "Roti", "Salad", "Milk"];
     return (
-        // Fragment
-        <> 
+        <>
             <h1>Healthy Food</h1>
             <ul className="list-group">
                 {foodItems.map((item) => {
-                    return <li key={item} className="list-group-item">{item}</li>
+                    return (
+                        <li key={item} className="list-group-item">
+                            {item}
+                        </li>
+                    );
                 })}
             </ul>
         </>
