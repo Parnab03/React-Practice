48fe39ee8e624ad06babd9e96e9227ef7713a954
Parnab03 <pb7439578071@gmail.com>
2024-07-23 12:39:52 +0530

    Conditional Rendaring added

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -2,10 +2,16 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    let foodItems = ["Dal", "Vegiii", "Roti", "Salad", "Milk"];
+    let foodItems = [];
+    // let foodItems = ["Dal", "Vegiii", "Roti", "Salad", "Milk"];
+
+    // if (foodItems.length === 0) {
+    //     return <h1>No Items Found</h1>;
+    // }
     return (
         <>
             <h1>Healthy Food</h1>
+            {foodItems.length === 0 ? <h3>No Items Found</h3> : null}
             <ul className="list-group">
                 {foodItems.map((item) => {
                     return (
