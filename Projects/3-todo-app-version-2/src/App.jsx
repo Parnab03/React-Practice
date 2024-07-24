a601e0bb0ac212d49662c4b76a2c2a1042e6c85c
Parnab03 <pb7439578071@gmail.com>
2024-07-24 20:50:28 +0530

    todo app version 2 started

---
+++ Projects/3-todo-app-version-2/src/App.jsx
@@ -0,0 +1,20 @@
+import AppName from "./Components/AppName";
+import AddTodo from "./Components/AddTodo";
+import Todoitem1 from "./Components/Todoitem1";
+import Todoitem2 from "./Components/Todoitem2";
+import "./App.css";
+
+function App() {
+    return (
+        <center className="todo-container">
+            <AppName></AppName>
+            <AddTodo></AddTodo>
+            <div className="items-container">
+                <Todoitem1></Todoitem1>
+                <Todoitem2></Todoitem2>
+            </div>
+        </center>
+    );
+}
+
+export default App;
