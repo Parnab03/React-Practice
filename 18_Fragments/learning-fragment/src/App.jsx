75e70ea8c5e28426ca537140d62fb33d284f3b07
Parnab Bagchi <pb7439578071@gmail.com>
2024-08-25 09:40:57 +0530

    18 to 27 added

--- 18_Fragments/learning-fragment/src/App.jsx
+++ 18_Fragments/learning-fragment/src/App.jsx
@@ -1,21 +1,33 @@
-import "./App.css";
+import FoodItems from "./components/FoodItems";
+import ErrorMessage from "./components/ErrorMessage";
 import "bootstrap/dist/css/bootstrap.min.css";
-import FoodItems from "./Components/FoodItems";
+import "./App.css";
+import Container from "./components/Container";
+import FoodInput from "./components/FoodInput";
+import { useState } from "react";
 
 function App() {
-    // let foodItems = [];
-    let foodItems = ["Dal", "Vegiii", "Roti", "Salad", "Milk"];
+  let [foodItems, setFoodItems] = useState([]);
 
-    // if (foodItems.length === 0) {
-    //     return <h1>No Items Found</h1>;
-    // }
-    return (
-        <>
-            <h1>Healthy Food</h1>
-            {foodItems.length === 0 ? <h3>No Items Found</h3> : null}
-            <FoodItems foodItems={foodItems}></FoodItems>
-        </>
-    );
+  const onKeyDown = (event) => {
+    if (event.key === "Enter") {
+      let newFoodItem = event.target.value;
+      event.target.value = "";
+      let newItems = [...foodItems, newFoodItem];
+      setFoodItems(newItems);
+    }
+  };
+
+  return (
+    <>
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
+        <FoodInput handleKeyDown={onKeyDown}></FoodInput>
+        <ErrorMessage items={foodItems}></ErrorMessage>
+        <FoodItems items={foodItems}></FoodItems>
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- 18_Fragments/learning-fragment/src/Components/FoodItems.jsx
+++ 18_Fragments/learning-fragment/src/Components/FoodItems.jsx
@@ -1,15 +1,26 @@
-const FoodItems = () => {
-    return (
-        <ul className="list-group">
-            {FoodItems.map((item) => {
-                return (
-                    <li key={item} className="list-group-item">
-                        {item}
-                    </li>
-                );
-            })}
-        </ul>
-    );
+import { useState } from "react";
+import Item from "./Item";
+
+const FoodItems = ({ items }) => {
+  let [activeItems, setActiveItems] = useState([]);
+
+  let onBuyButton = (item, event) => {
+    let newItems = [...activeItems, item];
+    setActiveItems(newItems);
+  };
+
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <Item
+          key={item}
+          foodItem={item}
+          bought={activeItems.includes(item)}
+          handleBuyButton={(event) => onBuyButton(item, event)}
+        ></Item>
+      ))}
+    </ul>
+  );
 };
 
 export default FoodItems;
